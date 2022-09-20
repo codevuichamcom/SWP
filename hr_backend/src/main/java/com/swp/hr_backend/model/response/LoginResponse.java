63e658a337da0e7b0f502b154d00893277f88de0
Thuong <thuongmc2k2@gmail.com>
2022-09-20 07:42:34 +0700

    HR_System commit 1st

---
+++ hr_backend/src/main/java/com/swp/hr_backend/model/response/LoginResponse.java
@@ -0,0 +1,16 @@
+package com.swp.hr_backend.model.response;
+
+public class LoginResponse {
+    private int id;
+    private String username;
+    private String roleName;
+    private String firstName;
+    private String lastName;
+    private String url;
+    private String email;
+    private String phone;
+    private boolean gender;
+    private int status;
+    private String token;
+    private String refreshToken;
+}
